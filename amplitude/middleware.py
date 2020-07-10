7acdd2a2d7bdeaa4ae2809b8dcaed4630095f671
Matt Pye <pyematt@gmail.com>
2020-07-10 15:54:25 +0100

    Catch AttributeError from amplitude.page_view_event in middleware

--- amplitude/middleware.py
+++ amplitude/middleware.py
@@ -8,5 +8,12 @@ class SendPageViewEvent(object):
         self.get_response = get_response
 
     def __call__(self, request):
-        amplitude.page_view_event(request)
+        try:
+            """
+            Catches 'WSGIRequest' object has no attribute 'user' or 'session'
+            if django.contrib.auth or django.contrib.sessions not in INSTALLED_APPS
+            """  # NOQA: E501
+            amplitude.page_view_event(request)
+        except AttributeError:
+            pass
         return self.get_response(request)
