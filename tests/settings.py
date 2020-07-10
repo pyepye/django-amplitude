7a571d54e1a4d1fd3d48c403d695ca12f6b006f2
Matt Pye <pyematt@gmail.com>
2020-07-10 10:07:55 +0100

    Fixup tox

--- tests/settings.py
+++ tests/settings.py
@@ -13,25 +13,16 @@ DATABASES = {
 
 ROOT_URLCONF = 'tests.urls'
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    # 'django.contrib.messages',
-    # 'django.contrib.sites',
-    # 'django.contrib.staticfiles',
     'amplitude',
     'tests',
 ]
 
 MIDDLEWARE = (
-    # 'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'amplitude.middleware.SendPageViewEvent',
 )
 
