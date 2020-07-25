98a3a362e923eb607d83598a50d7d9cfe5e9c532
Matt Pye <pyematt@gmail.com>
2020-07-25 12:10:34 +0100

    Use HttpRequest in test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,4 @@
-from django.urls import reverse
-
+from django.http import HttpRequest
 from amplitude.utils import get_client_ip, get_user_agent
 
 
@@ -7,9 +6,7 @@ def test_get_client_ip(client, settings):
     if 'amplitude.middleware.SendPageViewEvent' in settings.MIDDLEWARE:
         settings.MIDDLEWARE.remove('amplitude.middleware.SendPageViewEvent')
 
-    url = reverse('test_home')
-    response = client.get(url)
-    request = response.wsgi_request
+    request = HttpRequest()
     get_client_ip(request)
 
 
@@ -17,9 +14,7 @@ def test_get_client_ip_no_meta(client, settings):
     if 'amplitude.middleware.SendPageViewEvent' in settings.MIDDLEWARE:
         settings.MIDDLEWARE.remove('amplitude.middleware.SendPageViewEvent')
 
-    url = reverse('test_home')
-    response = client.get(url)
-    request = response.wsgi_request
+    request = HttpRequest()
     delattr(request, 'META')
     ip = get_client_ip(request)
     assert ip == ''
@@ -29,9 +24,7 @@ def test_get_client_ip_x_forwarded_for(client, settings):
     if 'amplitude.middleware.SendPageViewEvent' in settings.MIDDLEWARE:
         settings.MIDDLEWARE.remove('amplitude.middleware.SendPageViewEvent')
 
-    url = reverse('test_home')
-    response = client.get(url)
-    request = response.wsgi_request
+    request = HttpRequest()
     correct_ip = '70.41.3.18'
     request.META['HTTP_X_FORWARDED_FOR'] = f'203.0.113.195, {correct_ip}'
     ip = get_client_ip(request)
@@ -42,9 +35,7 @@ def test_get_user_agent(client, settings):
     if 'amplitude.middleware.SendPageViewEvent' in settings.MIDDLEWARE:
         settings.MIDDLEWARE.remove('amplitude.middleware.SendPageViewEvent')
 
-    url = reverse('test_home')
-    response = client.get(url)
-    request = response.wsgi_request
+    request = HttpRequest()
     request.META['HTTP_USER_AGENT'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1'  # NOQA: E501
     user_agent = get_user_agent(request)
     assert str(user_agent) == 'PC / Ubuntu / Firefox 15.0.1'
@@ -54,9 +45,7 @@ def test_get_user_agent_no_meta(client, settings):
     if 'amplitude.middleware.SendPageViewEvent' in settings.MIDDLEWARE:
         settings.MIDDLEWARE.remove('amplitude.middleware.SendPageViewEvent')
 
-    url = reverse('test_home')
-    response = client.get(url)
-    request = response.wsgi_request
+    request = HttpRequest()
     delattr(request, 'META')
     user_agent = get_user_agent(request)
     assert user_agent == ''
@@ -66,9 +55,7 @@ def test_get_user_agent_no_http_user_agent(client, settings):
     if 'amplitude.middleware.SendPageViewEvent' in settings.MIDDLEWARE:
         settings.MIDDLEWARE.remove('amplitude.middleware.SendPageViewEvent')
 
-    url = reverse('test_home')
-    response = client.get(url)
-    request = response.wsgi_request
+    request = HttpRequest()
     user_agent = get_user_agent(request)
     assert str(user_agent) == 'Other / Other / Other'
 
@@ -77,9 +64,7 @@ def test_get_user_agent_known_user_agent(client, settings):
     if 'amplitude.middleware.SendPageViewEvent' in settings.MIDDLEWARE:
         settings.MIDDLEWARE.remove('amplitude.middleware.SendPageViewEvent')
 
-    url = reverse('test_home')
-    response = client.get(url)
-    request = response.wsgi_request
+    request = HttpRequest()
     request.META['HTTP_USER_AGENT'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1'  # NOQA: E501
     get_user_agent(request)
     user_agent = get_user_agent(request)
