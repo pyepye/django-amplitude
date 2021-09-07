c338ea5d87246c5c2022e4ef3942de4bc24b0b21
Matt Pye <pyematt@gmail.com>
2021-09-07 23:36:45 +0100

    Update deps and remove django warning

--- amplitude/__init__.py
+++ amplitude/__init__.py
@@ -1,3 +1,13 @@
+from django import get_version
+from packaging import version
+
 from .amplitude import Amplitude  # NOQA: F401
 
-default_app_config = "amplitude.apps.AmplitudeConfig"
+"""
+To stop waring:
+    RemovedInDjango41Warning: 'amplitude' defines
+    default_app_config = 'amplitude.apps.AmplitudeConfig'. Django now detects
+    this configuration automatically. You can remove default_app_config.
+"""
+if version.parse(get_version()) < version.parse('3.2'):  # pragma: no cover
+    default_app_config = "amplitude.apps.AmplitudeConfig"
